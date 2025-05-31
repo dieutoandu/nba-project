08755c7e73807333a4fdbc83bbc6ac9488eb05e1
dieutoandu <dieutoandu@yahoo.com>
2025-05-31 23:07:03 +0800

    day 5/31__

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def player_data():
     df.columns = ['id','姓名', '位置', '上場數', '場均時間', '平均得分', '籃板', '助攻', '抄截', '阻攻','投籃命中率', '三分命中率', '罰球命中率', '失誤', '犯規']
 
     if player_data in df.columns.tolist():
-        df=df.sort_values(by=player_data,ascending=False)[:5]
+        df=df.sort_values(by=player_data,ascending=False)[:10]
         all_player_name=df['姓名'].values.tolist()
         EFF=eff(df)
         # print(df)
@@ -110,7 +110,7 @@ def player_radar():
     score=[]
     player_name=[]
     if player_data in df.columns.tolist():
-        df=df.sort_values(by=player_data,ascending=False)[:5]
+        df=df.sort_values(by=player_data,ascending=False)[:10]
         score=df[['平均得分','籃板','助攻','抄截','阻攻','失誤']].astype(float).values.tolist()
         player_name=df['姓名'].values.tolist()
     
